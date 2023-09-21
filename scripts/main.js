989ee5ae2cbd25ba5c5b542c68e3a7cd5856b2ff
anderssongodoy <anderssongodoygarcia@gmail.com>
2023-09-20 19:44:43 -0500

    Subiendo NavBar

--- scripts/main.js
+++ scripts/main.js
@@ -1,91 +1,66 @@
-// Seleccionar el formulario
 const form = document.getElementById('contact-form');
+    const nombreInput = document.getElementById('nombre');
+    const correoInput = document.getElementById('correo');
+    const asuntoInput = document.getElementById('asunto');
+    const mensajeInput = document.getElementById('mensaje');
+    const submitButton = document.getElementById('submit-button');
 
-// Función para validar el formulario
-function validateForm() {
-    // Obtener los campos del formulario
-    const nombre = document.getElementById('nombre');
-    const email = document.getElementById('email');
-    const mensaje = document.getElementById('mensaje');
+    // Función para validar el formulario
+    function validarFormulario() {
+      let valido = true;
 
-    // Validar el campo Nombre
-    if (nombre.value.trim() === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, ingrese su nombre',
-        });
-        return false;
+      // Validar campo Nombre
+      const nombreValue = nombreInput.value.trim();
+      if (nombreValue === '' || nombreValue.length > 50) {
+        document.getElementById('nombre-error').textContent = 'El nombre es obligatorio y debe tener menos de 50 caracteres.';
+        valido = false;
+      } else {
+        document.getElementById('nombre-error').textContent = '';
+      }
+
+      // Validar campo Correo
+      const correoValue = correoInput.value.trim();
+      const correoRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (correoValue === '' || !correoRegExp.test(correoValue)) {
+        document.getElementById('correo-error').textContent = 'El correo no es válido.';
+        valido = false;
+      } else {
+        document.getElementById('correo-error').textContent = '';
+      }
+
+      // Validar campo Asunto
+      const asuntoValue = asuntoInput.value.trim();
+      if (asuntoValue === '' || asuntoValue.length > 50) {
+        document.getElementById('asunto-error').textContent = 'El asunto es obligatorio y debe tener menos de 50 caracteres.';
+        valido = false;
+      } else {
+        document.getElementById('asunto-error').textContent = '';
+      }
+
+      // Validar campo Mensaje
+      const mensajeValue = mensajeInput.value.trim();
+      if (mensajeValue === '' || mensajeValue.length > 300) {
+        document.getElementById('mensaje-error').textContent = 'El mensaje es obligatorio y debe tener menos de 300 caracteres.';
+        valido = false;
+      } else {
+        document.getElementById('mensaje-error').textContent = '';
+      }
+
+      // Habilitar o deshabilitar el botón de enviar según la validación
+      submitButton.disabled = !valido;
+
+      return valido;
     }
 
-    // Validar el campo Correo Electrónico
-    if (email.value.trim() === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, ingrese su correo electrónico',
-        });
-        return false;
-    } else if (!isValidEmail(email.value.trim())) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, ingrese un correo electrónico válido',
-        });
-        return false;
-    }
-
-    // Validar el campo Mensaje
-    if (mensaje.value.trim() === '') {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, ingrese su mensaje',
-        });
-        return false;
-    }
-
-    // Si todos los campos son válidos, retornar verdadero
-    return true;
-}
-
-// Función para validar si un correo electrónico es válido
-function isValidEmail(email) {
-    // Expresión regular para validar correos electrónicos
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    return emailRegex.test(email);
-}
-
-// Agregar el evento submit al formulario
-form.addEventListener('submit', function (event) {
-    // Prevenir el envío del formulario
-    event.preventDefault();
-
-    // Validar el formulario
-    if (validateForm()) {
-        // Si el formulario es válido, enviarlo
-        submitForm();
-    }
-});
-
-// Función para enviar el formulario
-function submitForm() {
-    // Obtener los campos del formulario
-    const nombre = document.getElementById('nombre');
-    const email = document.getElementById('email');
-    const mensaje = document.getElementById('mensaje');
-
-    // AJAX
-
-    // Mostrar un mensaje de confirmación al usuario
-    Swal.fire({
-        icon: 'success',
-        title: '¡Mensaje enviado!',
-        text: 'Gracias por contactarnos. Le responderemos lo antes posible.',
-        confirmButtonText: 'Aceptar'
+    // Agregar un evento de escucha al formulario
+    form.addEventListener('submit', function (e) {
+      if (!validarFormulario()) {
+        e.preventDefault(); // Evitar el envío del formulario si no es válido
+      }
     });
 
-    // Limpiar los campos del formulario
-    form.reset();
-}
+    // Agregar eventos de escucha a los campos de entrada para validar en tiempo real
+    nombreInput.addEventListener('input', validarFormulario);
+    correoInput.addEventListener('input', validarFormulario);
+    asuntoInput.addEventListener('input', validarFormulario);
+    mensajeInput.addEventListener('input', validarFormulario);
---
+++ scripts/typing.js
@@ -0,0 +1,17 @@
+
+// Configura las opciones
+const options = {
+    strings: ['Hola Soy Andersson Godoy. </br> Soy Desarrollador ', "Este es mi portafolio.", "Espero sea de su agrado."],
+    typeSpeed: 50,
+    backSpeed: 30,
+    startDelay: 500,
+    backDelay: 500,
+    loop: true,
+    loopCount: Infinity,
+};
+
+// Inicializa Typed.js
+const typed = new Typed("#typed-output", options);
+
+
+
